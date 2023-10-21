4ff4f215656b18570510d895d9f675d1f710f4f7
jetzerszeri <jetzers.2@gmail.com>
2023-10-20 22:04:08 -0400

    feat: add active class when clicking a li

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log('hola desde console.log');
+//bottom navbar
+const bottomNavListItems = document.querySelectorAll('.bottomNavBar ul li');
+bottomNavListItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+        bottomNavListItems.forEach(function (item) {
+            item.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
