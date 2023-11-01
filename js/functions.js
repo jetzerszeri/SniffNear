e2fa8c3b4ccefc3fcf678c183e29247498b203d0
jetzerszeri <jetzers.2@gmail.com>
2023-10-31 22:25:53 -0400

    feat: add pet to the db with the new form
    missing the img

--- js/functions.js
+++ js/functions.js
@@ -101,5 +101,52 @@ function addValidationEvent(input) {
 }
 
 
+function calculateAge(isoDate) {
+    const birthDate = new Date(isoDate);
+    const currentDate = new Date();
 
-export { markSomethingAsSelectedWithHideInput, checkAndUncheckARadioInput, validateInput, addValidationEvent };
+    let years = currentDate.getFullYear() - birthDate.getFullYear();
+    let months = currentDate.getMonth() - birthDate.getMonth();
+
+    // Si el mes actual es menor que el mes de nacimiento, disminuye un año y suma 12 a los meses
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+    // Si estamos en el mismo mes pero el día actual es menor que el día de nacimiento, disminuye un mes
+    if (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() < birthDate.getDate()) {
+        months--;
+    }
+
+    // return {
+    //     years: years,
+    //     months: months
+    // };
+
+    let texto = `${years} años`;
+    switch (years) {
+        case 0:
+            texto = `${months} meses`;
+            break;
+        case 1:
+            if (months === 0) {
+                texto = `${years} año`;
+            } else {
+                texto = `${years} año y ${months} meses`;
+            }
+            break;
+        default:
+            if (months === 0) {
+                texto = `${years} años`;
+            } else {
+                texto = `${years} años y ${months} meses`;
+            }
+            break;
+    }
+
+    return texto;
+}
+
+
+export { markSomethingAsSelectedWithHideInput, checkAndUncheckARadioInput, validateInput, addValidationEvent, calculateAge };
